47ac11ac659afe5997cd2edd751a34b11bca24e2
Lubomir-Hlavina <lubo.hlavina@gmail.com>
2024-05-07 11:34:24 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from './pages';
+
+import { ErrorElement } from './components';
+
+// loaders
+import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
+// actions
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        loader: landingLoader,
+        errorElement: <ErrorElement />,
+      },
+      { path: 'products', element: <Products /> },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+        loader: singleProductLoader,
+        errorElement: <ErrorElement />,
+      },
+      { path: 'cart', element: <Cart /> },
+      { path: 'about', element: <About /> },
+      { path: 'checkout', element: <Checkout /> },
+      { path: 'orders', element: <Orders /> },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
